a238dd83bc3a3aa891d45633d451ca52830c5585
Igor Korsakov <clonkrullez@mail.ru>
2012-07-14 20:08:57 +0600

    Вывод таблицы рекордов
    И добавление результатов в неё.

--- src/save_result.php
+++ src/save_result.php
@@ -1,8 +1,17 @@
 <?php
 if (isset($_POST['score'])) { $score=$_POST['score']; if ($score =='') { unset($score);} }
-	if (isset($_SESSION['city'])) { $city=$_SESSION['city']; if ($city =='') { unset($city);} }
-	if (isset($_SESSION['name'])) { $name=$_SESSION['name']; if ($name =='') { unset($name);} }
-	if (isset($_SESSION['login'])) { $login = $_SESSION['login']; if ($login =='') { unset($login);} }
+if (isset($_SESSION['name'])) { $score=$_SESSION['name']; if ($name =='') { unset($name);} }
+
+    //защита от "инъекций" тегов в БД
+    $login = stripslashes($score);
+    $login = htmlspecialchars($score);
+ $password = stripslashes($name);
+    $password = htmlspecialchars($name);
+ //удаляем лишние пробелы
+    $login = trim($login);
+    $password = trim($password);
+	$city = trim($city);
+	$name = trim($name);
  // подключаемся к базе
     include ("bd.php"); 
  // проверка на существование
@@ -11,8 +20,10 @@ if (isset($_POST['score'])) { $score=$_POST['score']; if ($score =='') { unset($
     if (!empty($myrow['id'])) {
     echo ("<script>location.href='alert1.html'</script>"); 
     }
+	
  // если такого нет, то сохраняем данные
     $result2 = mysql_query ("INSERT INTO russian_table (login,name,score,city) VALUES('$login','$name','$score','$city')");
+	
     // Проверяем, есть ли ошибки
     if ($result2=='TRUE')
     {
